687e85a5eac94f06b2e6e08679cf6db2ee8d9d48
fellyph <fellyph.cintra@gmail.com>
2016-06-11 15:25:55 +0100

    addin json data and render Component

--- src/js/components.js
+++ src/js/components.js
@@ -1,8 +1,15 @@
+var data = [{"nome": "job 1", "url": "http://fellyph.com.br/", "empresa": "Empresa 1", "designer": "Artista 1", "tecnologias": ["HTML", "CSS", "JS"]},
+{"nome": "job 2", "url": "http://fellyph.com.br/", "empresa": "Empresa 2", "designer": "Artista 2", "tecnologias": ["HTML", "CSS", "JS"]},
+{"nome": "job 3", "url": "http://fellyph.com.br/", "empresa": "Empresa 3", "designer": "Artista 3", "tecnologias": ["HTML", "CSS", "JS"]},
+{"nome": "job 4", "url": "http://fellyph.com.br/", "empresa": "Empresa 4", "designer": "Artista 4", "tecnologias": ["HTML", "CSS", "JS"]},
+{"nome": "job 5", "url": "http://fellyph.com.br/", "empresa": "Empresa 5", "designer": "Artista 5", "tecnologias": ["HTML", "CSS", "JS"]},
+{"nome": "job 6", "url": "http://fellyph.com.br/", "empresa": "Empresa 6", "designer": "Artista 6", "tecnologias": ["HTML", "CSS", "JS"]}]
+
 class PortfolioItem extends React.Component {
   render() {
     return (
       <article class="job">
-        <figure class="">
+        <figure>
           <img src="http://www.fellyph.com.br/blog/wp-content/uploads/2016/05/blog-elo7-1.png" alt="blog elo7" />
         </figure>
         <h4 class="title">Blog elo7</h4>
@@ -24,3 +31,7 @@ class PortfolioList extends React.Component {
     )
   }
 }
+
+ReactDOM.render(
+  <PortfolioList />, document.getElementById('portfolio')
+);
